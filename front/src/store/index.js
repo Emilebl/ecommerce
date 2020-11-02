bc60005c8915d30b240d0cf937570c4a542e834a
Emile Bl <bailay.emile@live.fr>
2020-11-02 17:49:41 +0100

    vuex added

---
+++ front/src/store/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store ({
+    state: {
+        currentValue: 0
+    },
+    mutations: {
+        INCREMENT_COUNTER(state) {
+            state.currentValue = state.currentValue + 10;
+        },
+        DECREMENT_COUNTER(state) {
+            state.currentValue = state.currentValue - 10;
+        }
+    },
+    actions: {
+
+    }
+})
